1f5af0b361a0049322d2bc1baa61db3268b66853
jorgequinterodoria <69540195+jorgequinterodoria@users.noreply.github.com>
2021-11-30 11:35:25 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,115 @@
+
+ ///show
+ function show(img1, img2, img3) {
+  const x = document.getElementById(img1);
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "visible";
+  }
+  const y = document.getElementById(img2);
+  if (y.style.visibility === "hidden") {
+    y.style.visibility = "visible";
+  } else {
+    y.style.visibility = "visible";
+  }
+
+  const q = document.getElementById(img3);
+  if (q.style.visibility === "hidden") {
+    q.style.visibility = "visible";
+  } else {
+    q.style.visibility = "visible";
+  }
+  
+}
+
+//Hide the popup01
+function hide() {
+  const x = document.getElementById("niegaJS");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
+
+//Hide the popup01
+function hide01() {
+  const x = document.getElementById("metocloJS");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
+
+//Hide the popup01
+function hide02() {
+  const x = document.getElementById("niega02JS");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
+
+//Hide the popup01
+function hide03() {
+  const x = document.getElementById("planiJS");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
+
+//Hide the popup01
+function hide04() {
+  const x = document.getElementById("hipoJS");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
+
+//Hide the popup01
+function hide05() {
+  const x = document.getElementById("consumoJS");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
+
+//Hide the popup01
+function hide06() {
+  const x = document.getElementById("q2_2");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
+
+
+//Hide the popup01
+function hide08() {
+  const x = document.getElementById("q2_1");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
+
+//Hide the popup01
+function hide08b() {
+  const x = document.getElementById("q2_2");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
