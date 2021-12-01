15e34a8a4a8a9bc1834a2913a9c718753f0c1922
jorgequinterodoria <69540195+jorgequinterodoria@users.noreply.github.com>
2021-12-01 11:00:50 -0500

    update functions Hide and Show

--- js/main.js
+++ js/main.js
@@ -1,34 +1,13 @@
 
  ///show
- function show(img1, img2, img3) {
-  const x = document.getElementById(img1);
-  if (x.style.visibility === "hidden") {
-    x.style.visibility = "visible";
-  } else {
-    x.style.visibility = "visible";
-  }
-  const y = document.getElementById(img2);
-  if (y.style.visibility === "hidden") {
-    y.style.visibility = "visible";
-  } else {
-    y.style.visibility = "visible";
-  }
-
-  const q = document.getElementById(img3);
-  if (q.style.visibility === "hidden") {
-    q.style.visibility = "visible";
-  } else {
-    q.style.visibility = "visible";
-  }
-  
+ function show(img1) {
+   const x = document.getElementById(img1);
+   x.style.visibility === "hidden" ? x.style.visibility = "visible" : x.style.visibility = "visible"
+   
 }
 
 //Hide the popup
 function hideP(popup) {
   const x = document.getElementById(popup);
-  if (x.style.visibility === "visible") {
-    x.style.visibility = "hidden";
-  } else {
-    x.style.visibility = "hidden";
-  }
+  x.style.visibility === "visible" ?  x.style.visibility = "hidden" : x.style.visibility ="hidden"
 }
